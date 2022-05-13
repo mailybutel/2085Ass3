3c6c72ffabc432a081e7bc231a8d30b9b100415f
Daniel Ding <ddin0018@student.monash.edu>
2022-05-13 22:14:05 +1000

    Fixed the hash

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from hash_table import LinearProbePotionTable
 from primes import largest_prime
 from potion import Potion
 
+hash = LinearProbePotionTable(100, True)
+health = Potion("Health Potion", "Milk", 15, 1)
+strength = Potion("Strength Potion", "Orange Juice", 20, 2)
 
-health = Potion("Health Potion", "Milk",15,1)
-strength = Potion("Strength Potion", )
+print(health.good_hash(health.name,largest_prime(100)))
